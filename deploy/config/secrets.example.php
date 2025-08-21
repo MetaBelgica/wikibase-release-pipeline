91a65d61061318812da50d44b15952550d51adf4
gcgbarbosa <gcgbarbosa@gmail.com>
2025-08-21 16:54:59 +0000

    chore: add example

---
+++ deploy/config/secrets.example.php
@@ -0,0 +1,13 @@
+<?php
+
+## Database settings
+$wgDBtype = "mysql";
+$wgDBserver = "mysql:3306";
+$wgDBname = "CHANGEME";
+$wgDBuser = "CHANGEME";
+$wgDBpassword = "CHANGEME";
+
+$wgSecretKey = "CHANGEME";
+# Site upgrade key. Must be set to a string (default provided) to turn on the
+# web installer while LocalSettings.php is in place
+$wgUpgradeKey = "CHANGEME";
